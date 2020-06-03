c2e16eba9d5825f0c167cd1ef0ab214d5a4ec8c3
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-06-03 14:32:56 -0400

    lwIP driver Rx data buffers not aligned to cache line size -- slower

--- LWIP/Target/ethernetif.c
+++ LWIP/Target/ethernetif.c
@@ -48,22 +48,26 @@
 #if defined ( __ICCARM__ ) /*!< IAR Compiler */
   #pragma data_alignment=4   
 #endif
-__ALIGN_BEGIN ETH_DMADescTypeDef  DMARxDscrTab[ETH_RXBUFNB] __ALIGN_END;/* Ethernet Rx MA Descriptor */
+//__ALIGN_BEGIN ETH_DMADescTypeDef  DMARxDscrTab[ETH_RXBUFNB] __ALIGN_END;/* Ethernet Rx MA Descriptor */
+ALIGN_32BYTES(ETH_DMADescTypeDef  DMARxDscrTab[ETH_RXBUFNB]);
 
 #if defined ( __ICCARM__ ) /*!< IAR Compiler */
   #pragma data_alignment=4   
 #endif
-__ALIGN_BEGIN ETH_DMADescTypeDef  DMATxDscrTab[ETH_TXBUFNB] __ALIGN_END;/* Ethernet Tx DMA Descriptor */
+//__ALIGN_BEGIN ETH_DMADescTypeDef  DMATxDscrTab[ETH_TXBUFNB] __ALIGN_END;/* Ethernet Tx DMA Descriptor */
+ALIGN_32BYTES(ETH_DMADescTypeDef  DMATxDscrTab[ETH_TXBUFNB]);
 
 #if defined ( __ICCARM__ ) /*!< IAR Compiler */
   #pragma data_alignment=4   
 #endif
-__ALIGN_BEGIN uint8_t Rx_Buff[ETH_RXBUFNB][ETH_RX_BUF_SIZE] __ALIGN_END; /* Ethernet Receive Buffer */
+//__ALIGN_BEGIN uint8_t Rx_Buff[ETH_RXBUFNB][ETH_RX_BUF_SIZE] __ALIGN_END; /* Ethernet Receive Buffer */
+ALIGN_32BYTES(uint8_t Rx_Buff[ETH_RXBUFNB][ETH_RX_BUF_SIZE]);
 
 #if defined ( __ICCARM__ ) /*!< IAR Compiler */
   #pragma data_alignment=4   
 #endif
-__ALIGN_BEGIN uint8_t Tx_Buff[ETH_TXBUFNB][ETH_TX_BUF_SIZE] __ALIGN_END; /* Ethernet Transmit Buffer */
+//__ALIGN_BEGIN uint8_t Tx_Buff[ETH_TXBUFNB][ETH_TX_BUF_SIZE] __ALIGN_END; /* Ethernet Transmit Buffer */
+ALIGN_32BYTES(uint8_t Tx_Buff[ETH_TXBUFNB][ETH_TX_BUF_SIZE]);
 
 /* USER CODE BEGIN 2 */
 
