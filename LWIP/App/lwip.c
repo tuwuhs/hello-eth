1dae34df2810872225e90fa19cd34e534bbc5700
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-06-02 20:37:51 -0400

    Change to DHCP, it works if the interface is up when powering up

--- LWIP/App/lwip.c
+++ LWIP/App/lwip.c
@@ -32,6 +32,9 @@
 /* ETH Variables initialization ----------------------------------------------*/
 void Error_Handler(void);
 
+/* DHCP Variables initialization ---------------------------------------------*/
+uint32_t DHCPfineTimer = 0;
+uint32_t DHCPcoarseTimer = 0;
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
@@ -41,9 +44,6 @@ struct netif gnetif;
 ip4_addr_t ipaddr;
 ip4_addr_t netmask;
 ip4_addr_t gw;
-uint8_t IP_ADDRESS[4] = {192, 168, 137, 90};
-uint8_t NETMASK_ADDRESS[4] = {255, 255, 255, 0};
-uint8_t GATEWAY_ADDRESS[4] = {192, 168, 137, 1};
 
 /* USER CODE BEGIN 2 */
 
@@ -54,14 +54,13 @@ uint8_t GATEWAY_ADDRESS[4] = {192, 168, 137, 1};
   */
 void MX_LWIP_Init(void)
 {
-  /* IP addresses initialization */
   /* Initilialize the LwIP stack without RTOS */
   lwip_init();
 
-  /* IP addresses initialization without DHCP (IPv4) */
-  IP4_ADDR(&ipaddr, IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);
-  IP4_ADDR(&netmask, NETMASK_ADDRESS[0], NETMASK_ADDRESS[1] , NETMASK_ADDRESS[2], NETMASK_ADDRESS[3]);
-  IP4_ADDR(&gw, GATEWAY_ADDRESS[0], GATEWAY_ADDRESS[1], GATEWAY_ADDRESS[2], GATEWAY_ADDRESS[3]);
+  /* IP addresses initialization with DHCP (IPv4) */
+  ipaddr.addr = 0;
+  netmask.addr = 0;
+  gw.addr = 0;
 
   /* add the network interface (IPv4/IPv6) without RTOS */
   netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &ethernet_input);
@@ -79,6 +78,9 @@ void MX_LWIP_Init(void)
     /* When the netif link is down this function must be called */
     netif_set_down(&gnetif);
   }
+
+  /* Start DHCP negotiation for a network interface (IPv4) */
+  dhcp_start(&gnetif);
 
 /* USER CODE BEGIN 3 */
 
